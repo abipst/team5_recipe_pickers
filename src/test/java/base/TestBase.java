33ae22a025c747db96e98bbda556fb824bef3049
kavitapakhale <97461496+kavitapakhale@users.noreply.github.com>
2024-07-16 23:34:38 -0400

    Added code to write data into excel

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,11 +1,13 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utils.LoggerLoad;
 
 import java.io.FileNotFoundException;
